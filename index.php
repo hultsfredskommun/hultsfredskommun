8f3c9295bf016845a61440211c195f6878791247
Erik Lindgren <erik.lindgren@campushultsfred.se>
2012-07-31 11:38:17 +0200

    index.php clean-up

--- index.php
+++ index.php
@@ -14,39 +14,46 @@
 
 get_header(); ?>
 
-		<div id="primary">
-			<div id="content" role="main">
+<div id="primary">
+	<div id="firstpage-top-content">
+		<?php dynamic_sidebar('firstpage-top-content'); ?>
+	</div>
+	
+	<div id="content" role="main">
+		<div id="sticky-posts">
+			<?php 
+				/* Get all sticky posts */
+				$sticky = get_option( 'sticky_posts' );
+				
+				/* Sort the stickies with the newest ones at the top */
+				rsort( $sticky );
+				
+				/* Get the 2 newest stickies (change 2 for a different number) */
+				$sticky = array_slice( $sticky, 0, 2 );
 
-			<?php if ( have_posts() ) : ?>
+				/* Query sticky posts */
+				query_posts( array( 'post__in' => $sticky, /*'cat__in' => ,*/ 'caller_get_posts' => 1 ) );
+			?>
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>			
+				<div <?php post_class(); ?>>
+					<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+					<?php the_content(); ?>
+				</div>
+			<?php endwhile; else: ?>
+				<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+			<?php endif; 
+				// Reset Query
+				wp_reset_query(); 
+			?>
+		</div><!-- #sticky-posts -->
+		
+		<?php dynamic_sidebar('firstpage-content'); ?>
+	</div><!-- #content -->
+	
+	<div id="firstpage-sidebar">
+		<?php dynamic_sidebar('firstpage-sidebar'); ?>
+	</div>
+</div><!-- #primary -->
 
-				<?php twentyeleven_content_nav( 'nav-above' ); ?>
-
-				<?php /* Start the Loop */ ?>
-				<?php while ( have_posts() ) : the_post(); ?>
-
-					<?php get_template_part( 'content', get_post_format() ); ?>
-
-				<?php endwhile; ?>
-
-				<?php twentyeleven_content_nav( 'nav-below' ); ?>
-
-			<?php else : ?>
-
-				<article id="post-0" class="post no-results not-found">
-					<header class="entry-header">
-						<h1 class="entry-title"><?php _e( 'Nothing Found', 'twentyeleven' ); ?></h1>
-					</header><!-- .entry-header -->
-
-					<div class="entry-content">
-						<p><?php _e( 'Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.', 'twentyeleven' ); ?></p>
-						<?php get_search_form(); ?>
-					</div><!-- .entry-content -->
-				</article><!-- #post-0 -->
-
-			<?php endif; ?>
-
-			</div><!-- #content -->
-		</div><!-- #primary -->
-
-<?php get_sidebar(); ?>
+<?php// get_sidebar(); ?>		
 <?php get_footer(); ?>
