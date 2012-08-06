820debbd8dad1bb782ffe203e7bd0b5cff2ae4c6
Jonas Hjalmarsson <jonas.hjalmarsson@hultsfred.se>
2012-08-06 10:39:03 +0200

    added filter to remove width and height attr in img tag

--- functions.php
+++ functions.php
@@ -98,6 +98,13 @@ if ( function_exists( 'add_image_size' ) ) {
 	add_image_size( 'contact-image',  $default_settings['contact-image'][0], $default_settings['contact-image'][1], $default_settings['contact-image'][2] );
 }
 
+// remove width and height attr on img tags
+function remove_thumbnail_dimensions( $html ) {
+    $html = preg_replace( '/(width|height)=\"\d*\"\s/', "", $html );
+    return $html;
+}
+add_filter( 'post_thumbnail_html', 'remove_thumbnail_dimensions', 10 );
+add_filter( 'image_send_to_editor', 'remove_thumbnail_dimensions', 10 );
 
 
 /**
