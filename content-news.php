6fbbb814920fad7e0ebb4f97cd6e63017b2a22ef
Erik Lindgren <erik.lindgren@campushultsfred.se>
2012-08-09 08:23:35 +0200

    style and news fix

--- content-news.php
+++ content-news.php
@@ -11,23 +11,29 @@
 		<div class="readMoreContainer">
 			<div class="readMoreContent">
 				<div class="summary-content">
-					<?php the_post_thumbnail('thumbnail-image'); ?>
-					<header class="entry-header">
-						<?php if ( is_sticky() ) : ?>
-							<hgroup>
-								<h2 class="entry-title"><a post_id="<?php the_ID(); ?>" href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyeleven' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-								<h3 class="entry-format"><?php _e( 'Featured', 'twentyeleven' ); ?></h3>
-							</hgroup>
-						<?php else : ?>
-							<h1 class="entry-title"><a post_id="<?php the_ID(); ?>" href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyeleven' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
-						<?php endif; ?>
-					</header><!-- .entry-header -->
-					<div class="entry-content">
-						<?php the_excerpt(); ?>
-					</div><!-- .entry-content -->
+					<?php if (has_post_thumbnail()) : ?>
+						<div class="img-wrapper">
+							<?php the_post_thumbnail('thumbnail-image'); ?>
+						</div>
+					<?php endif; ?>
+					<div class="entry-wrapper <?php echo (has_post_thumbnail()) ? "has-img" : "";  ?>" >
+						<header class="entry-header">
+							<?php if ( is_sticky() ) : ?>
+								<hgroup>
+									<h2 class="entry-title"><a post_id="<?php the_ID(); ?>" href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyeleven' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+									<h3 class="entry-format"><?php _e( 'Featured', 'twentyeleven' ); ?></h3>
+								</hgroup>
+							<?php else : ?>
+								<h1 class="entry-title"><a post_id="<?php the_ID(); ?>" href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyeleven' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+							<?php endif; ?>
+						</header><!-- .entry-header -->
+						<div class="entry-content">
+							<?php the_excerpt(); ?>
+						</div><!-- .entry-content -->
+					</div><!-- .entry-wrapper -->
 					<div class="readMoreFadeBottom"></div>
-				</div>
-			</div>
-		</div>
+				</div><!-- .summary-content -->
+			</div><!-- .readMoreContent -->
+		</div><!-- .readMoreContainer -->
 		<a class="readMoreToggleButton" href="<?php the_permalink(); ?>">. . .</a>
 	</article><!-- #post-<?php the_ID(); ?> -->
