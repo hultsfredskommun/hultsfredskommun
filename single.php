6e62469cfbb325a021c0d988eba03ed1d853ab73
Jonas Hjalmarsson <jonas.hjalmarsson@hultsfred.se>
2012-08-13 12:08:54 +0200

    more posts below single post from same category..

--- single.php
+++ single.php
@@ -17,10 +17,87 @@ get_header(); ?>
 					<?php get_template_part( 'content', 'single' ); ?>
 
 					<?php comments_template( '', true ); ?>
+					<?php
+						// get category and tags from post to query related posts later.. 
+						$categories = get_the_category();
+						$cat_array = array();
+						if ($categories) { foreach ($categories as $value) {
+							$cat_array[] = $value->term_id;	
+						} } 
+						
+						$tags = get_the_tags(); 
+						$tag_array = array();
+						if ($tags) { foreach ($tags as $value) {
+							$tag_array[] = $value->term_id;	
+						} } 
+						
+						$ort = get_the_terms(get_the_ID(), "ort"); 					
+						$ort_array = array();
+						if ($ort) { foreach ($ort as $value) {
+							$ort_array[] = $value->term_id;	
+						} } 
 
+						$vem = get_the_terms(get_the_ID(), "vem"); 					
+						$vem_array = array();
+						if ($vem) { foreach ($vem as $value) {
+							$vem_array[] = $value->term_id;	
+						} } 
+					?>
 				<?php endwhile; // end of the loop. ?>
 
 			</div><!-- #content -->
+		<?php 
+		// check for all taxonomies in this query
+		$query = array( 'category__in' => $cat_array,
+					 	//'tag__in' => $tag_array
+						);
+		/*
+		if (count($vem_array) > 0 && count($ort_array) > 0) {
+			$query['tax_query'] = array(
+				'relation' => 'AND',
+				array(
+					'taxonomy' => 'vem',
+					'field' => 'id',
+					'terms' => $vem_array )
+				,
+				array(
+					'taxonomy' => 'ort',
+					'field' => 'id',
+					'terms' => $ort_array
+				)
+			);
+		}						 	
+		else if (count($vem_array) > 0) {
+			$query['tax_query'] = array(
+				array(
+					'taxonomy' => 'vem',
+					'field' => 'id',
+					'terms' => $vem_array )
+			);
+		}						 	
+		else if (count($ort_array) > 0) {
+			$query['tax_query'] = array(
+				array(
+					'taxonomy' => 'ort',
+					'field' => 'id',
+					'terms' => $ort_array )
+			);
+		}
+
+		*/
+		// loop all posts to get all taxonomies used 
+		$wpq = new WP_Query($query);
+		if ($wpq->have_posts()) : 
+			echo "<aside id='related_posts'>";
+			while ($wpq->have_posts()) : $wpq->the_post();
+				get_template_part( 'content' );
+			endwhile;
+			echo "</aside>";
+		endif;
+		?>
+		<!--div id="dyn-posts-placeholder-'+ settings["pageNum"] +'" class="dyn-posts-placeholder"></div>
+		<p id="dyn-posts-load-posts"><a href="#">Ladda fler sidor</a></p>--
+
 		</div><!-- #primary -->
 
 <?php get_footer(); ?>
