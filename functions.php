0c4631891aa5a3b122ed72348e152e9ca68d4633
Erik Lindgren <erik.lindgren@campushultsfred.se>
2012-08-17 09:39:28 +0200

    added taxonomy addon to Advanced Custom Fields
    edited taxonomy add-on to select the categorys that post is published under

--- functions.php
+++ functions.php
@@ -81,15 +81,17 @@ function hk_setup() {
 	// Grab hk sort-order function
 	require( get_template_directory() . '/inc/hk-sort-order.php' );
 
-	// Add default posts and comments RSS feed links to <head>.
-	add_theme_support( 'automatic-feed-links' );
-
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menu( 'primary', __( 'Primary Menu', 'twentyeleven' ) );
+	
+	// Registers taxonomy add-on for Advanced Custom Fields
+	register_field('Tax_field', dirname(__File__) . '/inc/acf-tax.php');
 
 	// This theme uses Featured Images (also known as post thumbnails) for per-post/per-page Custom Header images
 	add_theme_support( 'post-thumbnails' );
-
+	
+	// Add default posts and comments RSS feed links to <head>.
+	add_theme_support( 'automatic-feed-links' );
 }
 endif; // hk_setup
 
