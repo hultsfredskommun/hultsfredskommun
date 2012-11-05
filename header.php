8adbe6e9467226b98662e477ca22c85f7d2ae35a
Jonas Hjalmarsson <jonas.hjalmarsson@hultsfred.se>
2012-11-05 13:31:47 +0100

    top-image style fix

--- header.php
+++ header.php
@@ -92,6 +92,9 @@ if ( is_home() && ( $locations = get_nav_menu_locations() ) && isset( $locations
 			background-repeat: no-repeat;
 			background-position: top center;
 		}
+		#branding {
+			background-color: transparent;
+		}
 		<?php endif; if ($hk_options["primary_width"]) : ?>
 		#primary {
 			width: <?php echo $hk_options["primary_width"]; ?>;
