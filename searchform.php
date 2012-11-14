c345321a47b1cb6837cf7d8481c609867b967a5f
Jonas Hjalmarsson <jonas.hjalmarsson@hultsfred.se>
2012-11-14 12:02:51 +0100

    search form fixes

--- searchform.php
+++ searchform.php
@@ -7,8 +7,9 @@
  * @since Twenty Eleven 1.0
  */
 ?>
+	
 	<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 		<label for="s" class="assistive-text"><?php _e( 'Search', 'twentyeleven' ); ?></label>
-		<input type="text" class="field" name="s" id="s" autocomplete="off" />
+		<div class="input-wrapper"><input type="text" class="field" name="s" id="s" autocomplete="off" /></div>
 		<input type="submit" class="submit" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', 'twentyeleven' ); ?>" />
 	</form>
