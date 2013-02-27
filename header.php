628e19902c626fa7440d0816540b474b40d88fcf
Jonas Hjalmarsson <jonas.hjalmarsson@hultsfred.se>
2013-02-27 15:44:05 +0100

    temp localhost style

--- header.php
+++ header.php
@@ -80,6 +80,7 @@ if ( is_home() && ( $locations = get_nav_menu_locations() ) && isset( $locations
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <link rel="stylesheet" type="text/css" media="all" href="<?php echo get_template_directory_uri() . "/style-lt-ie9.css"; ?>" />
+<link href="http://localhost/wm_local_css/style.css" rel="stylesheet">
 <![endif]-->
 <?php
 	/* We add some JavaScript to pages with the comment form
