554d53179b3ffee7baf6ee7a3c24f54787b15c1d
Jonas Hjalmarsson <jonas.hjalmarsson@hultsfred.se>
2013-03-12 15:32:45 +0100

    try bugfix again

--- inc/hk-category.php
+++ inc/hk-category.php
@@ -123,6 +123,7 @@
 					$no_top_space = (count($shownPosts) == 0)?" no-top-space":"";
 					echo "<div class='more-from-heading" . $no_top_space ."'><span>Mer från underkategorier</span></div>";
 					$args = array( 'posts_per_page' => -1 );
+					$children = array();
 					if ( $cat != "" ) {
 						$children =  hk_getChildrenIdArray($cat);
 						if ( !empty($children) ) {
@@ -130,16 +131,49 @@
 						}
 					}
 					
-					
-					if (!empty($sticky)) {
+					if ($cat != "" && !empty($children)) {
+						if (!empty($sticky)) {
+							if ($tag == "") {
+								$args["post_type"] = array('post');
+							}
+							else {
+								$args["post_type"] = array('post','attachment');
+							}
+							$args['post_status'] = 'publish';		
+							$args['post__in'] = $sticky;
+							if ( !empty($tag_array) ) {
+								if ($_REQUEST["tag_logic"] == "and") {
+									$args["tag_slug__and"] = $tag_array;
+								} else {
+									$args["tag_slug__in"] = $tag_array;
+								}
+							}
+							if (!empty($shownPosts)) {
+								$args['post__not_in'] = $shownPosts;
+							}
+							query_posts( $args );
+							if ( have_posts() ) : while ( have_posts() ) : the_post();
+								get_template_part( 'content', get_post_format());
+								$shownPosts[] = get_the_ID();
+							endwhile; endif;
+							wp_reset_query(); // Reset Query
+							echo "<span class='hidden debug'>sticky from children category . <br>".print_r($args,true)."</span>";
+						}
+						
+						/* Get all NOT sticky posts children of this category */
+						$args = array( 'category__in' => $children);
 						if ($tag == "") {
+							$args['posts_per_page'] = $posts_per_page;
 							$args["post_type"] = array('post');
 						}
 						else {
 							$args["post_type"] = array('post','attachment');
+							$args['posts_per_page'] = -1;
 						}
-						$args['post_status'] = 'publish';		
-						$args['post__in'] = $sticky;
+						$args['post_status'] = 'publish';
+						if ( !empty($sticky) || !empty($shownPosts)) {
+							$args['post__not_in'] = array_merge($sticky,$shownPosts);
+						}
 						if ( !empty($tag_array) ) {
 							if ($_REQUEST["tag_logic"] == "and") {
 								$args["tag_slug__and"] = $tag_array;
@@ -147,47 +181,14 @@
 								$args["tag_slug__in"] = $tag_array;
 							}
 						}
-						if (!empty($shownPosts)) {
-							$args['post__not_in'] = $shownPosts;
-						}
 						query_posts( $args );
 						if ( have_posts() ) : while ( have_posts() ) : the_post();
-							get_template_part( 'content', get_post_format());
+							get_template_part( 'content', get_post_format() );
 							$shownPosts[] = get_the_ID();
 						endwhile; endif;
 						wp_reset_query(); // Reset Query
-						echo "<span class='hidden debug'>sticky from children category . <br>".print_r($args,true)."</span>";
+						echo "<span class='hidden debug'>not sticky from children category . <br>".print_r($args,true)."</span>";
 					}
-					
-					/* Get all NOT sticky posts children of this category */
-					$args = array( 'category__in' => $children);
-					if ($tag == "") {
-						$args['posts_per_page'] = $posts_per_page;
-						$args["post_type"] = array('post');
-					}
-					else {
-						$args["post_type"] = array('post','attachment');
-						$args['posts_per_page'] = -1;
-					}
-					$args['post_status'] = 'publish';
-					if ( !empty($sticky) || !empty($shownPosts)) {
-						$args['post__not_in'] = array_merge($sticky,$shownPosts);
-					}
-					if ( !empty($tag_array) ) {
-						if ($_REQUEST["tag_logic"] == "and") {
-							$args["tag_slug__and"] = $tag_array;
-						} else {
-							$args["tag_slug__in"] = $tag_array;
-						}
-					}
-					query_posts( $args );
-					if ( have_posts() ) : while ( have_posts() ) : the_post();
-						get_template_part( 'content', get_post_format() );
-						$shownPosts[] = get_the_ID();
-					endwhile; endif;
-					wp_reset_query(); // Reset Query
-					echo "<span class='hidden debug'>not sticky from children category . <br>".print_r($args,true)."</span>";
-
 				
 				endif; 
 			endif;
