1d6318815eeb53da19c3587782f7e0536761fdcf
Jonas Hjalmarsson <jonas.hjalmarsson@hultsfred.se>
2013-05-07 10:34:55 +0200

    debug

--- inc/hk-sub-category-firstpage.php
+++ inc/hk-sub-category-firstpage.php
@@ -22,6 +22,6 @@
 <?php endif; ?>
 <?php if (is_active_sidebar('firstpage-pre-footer-sidebar')) : ?>
 <div id="firstpage-pre-footer-sidebar" class="firstpage-pre-footer-sidebar">
-<?php //dynamic_sidebar('firstpage-pre-footer-sidebar'); ?>
+	<?php dynamic_sidebar('firstpage-pre-footer-sidebar'); ?>
 </div><!-- #firstpage-pre-footer-sidebar -->
 <?php endif; ?>
