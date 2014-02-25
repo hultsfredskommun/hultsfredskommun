2736f05586106ecf5c3227cb62811a87bb80677e
hultsfredskommun <jonas.hjalmarsson@hultsfred.se>
2014-02-25 09:21:59 +0100

    php error widget removed

--- inc/hk-admin-pages.php
+++ inc/hk-admin-pages.php
@@ -273,62 +273,9 @@ function hk_cleanup_dashboard()
 		wp_add_dashboard_widget('hk_alllatestposts_dashboard_widget', 'Alla senaste ändringar', 'hk_display_alllatestposts_dashboard_widget' );
 		//All hidden posts
 		wp_add_dashboard_widget('hk_allhidden_dashboard_widget', 'Alla ej synliga sidor', 'hk_display_allhidden_dashboard_widget' );
-		
-		// Add widgets
-		wp_add_dashboard_widget( 'slt-php-errors', 'PHP errors', 'slt_PHPErrorsWidget' );
-    }
-
-}
-
-function slt_PHPErrorsWidget() {
-	$logfile = '/var/log/apache2/error.log'; // Enter the server path to your logs file here
-	$displayErrorsLimit = 100; // The maximum number of errors to display in the widget
-	$errorLengthLimit = 300; // The maximum number of characters to display for each error
-	$fileCleared = false;
-	$userCanClearLog = current_user_can( 'manage_options' );
-	// Clear file?
-	if ( $userCanClearLog && isset( $_GET["slt-php-errors"] ) && $_GET["slt-php-errors"]=="clear" ) {
-		$handle = fopen( $logfile, "w" );
-		fclose( $handle );
-		$fileCleared = true;
-	}
-	// Read file
-	if ( file_exists( $logfile ) ) {
-		$errors = file( $logfile );
-		$errors = array_reverse( $errors );
-		if ( $fileCleared ) echo '<p><em>File cleared.</em></p>';
-		if ( $errors ) {
-			echo '<p>'.count( $errors ).' error';
-			if ( $errors != 1 ) echo 's';
-			echo '.';
-			if ( $userCanClearLog ) echo ' [ <b><a href="'.get_bloginfo("url").'/wp-admin/?slt-php-errors=clear" onclick="return confirm(\'Are you sure?\');">CLEAR LOG FILE</a></b> ]';
-			echo '</p>';
-			echo '<div id="slt-php-errors" style="height:250px;overflow:scroll;padding:2px;background-color:#faf9f7;border:1px solid #ccc;">';
-			echo '<ol style="padding:0;margin:0;">';
-			$i = 0;
-			foreach ( $errors as $error ) {
-				echo '<li style="padding:2px 4px 6px;border-bottom:1px solid #ececec;">';
-				$errorOutput = preg_replace( '/\[([^\]]+)\]/', '<b>[$1]</b>', $error, 1 );
-				if ( strlen( $errorOutput ) > $errorLengthLimit ) {
-					echo substr( $errorOutput, 0, $errorLengthLimit ).' [...]';
-				} else {
-					echo $errorOutput;
-				}
-				echo '</li>';
-				$i++;
-				if ( $i > $displayErrorsLimit ) {
-					echo '<li style="padding:2px;border-bottom:2px solid #ccc;"><em>More than '.$displayErrorsLimit.' errors in log...</em></li>';
-					break;
-				}
-			}
-			echo '</ol></div>';
-		} else {
-			echo '<p>No errors currently logged.</p>';
-		}
-	} else {
-		echo '<p><em>There was a problem reading the error log file.</em></p>';
 	}
 }
+
 
 // function to display my coming reviews dashboard widget
 function hk_display_allcomingreviews_dashboard_widget ()
