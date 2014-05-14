6559ba9215ea0bb76d2a535a26ba1e64eeca368a
hultsfredskommun <jonas.hjalmarsson@hultsfred.se>
2014-05-14 10:07:30 +0200

    bugfix addthis admin

--- js/hultsfred-admin.js
+++ js/hultsfred-admin.js
@@ -15,7 +15,7 @@
 		//$("#content-add_media").attr("href",$("#content-add_media").attr("href") + "&tab=library");
 		
 		// add addthis share if in post wp-admin
-		if ($("body").hasClass("wp-admin") && $("body").hasClass("post-php") && hultsfred_object["addthis_pubid_admin"] != "") {
+		if ($("body").hasClass("wp-admin") && $("body").hasClass("post-php") && hultsfred_object["addthis_pubid_admin"] !== undefined) {
 			perma_link = $("#view-post-btn .button").attr("href");
 			var addthis_share = {
 				url: perma_link
