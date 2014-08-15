89732b5adcaa44aedf26904cbc22140be6675dd1
hultsfredskommun <jonas.hjalmarsson@hultsfred.se>
2014-08-15 10:27:09 +0200

    hk_version update

--- functions.php
+++ functions.php
@@ -11,7 +11,7 @@
  /**
   * Define HK_VERSION, will be set as version of style.css and hultsfred.js
   */
-define("HK_VERSION", "1.0.7");
+define("HK_VERSION", "1.0.8");
 
 /**
  * Set the content width based on the theme's design and stylesheet.
