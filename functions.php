a80d20bc922ec018aa6af0f781683d2f79cc574d
administrator <administrator@hks-webb.hultsfred.se>
2016-11-15 08:14:44 +0100

    quickpost style fix

--- functions.php
+++ functions.php
@@ -11,7 +11,7 @@
  /**
   * Define HK_VERSION, will be set as version of style.css and hultsfred.js
   */
-define("HK_VERSION", "4.9");
+define("HK_VERSION", "5.0");
 
 /**
  * Set the content width based on the theme's design and stylesheet.
