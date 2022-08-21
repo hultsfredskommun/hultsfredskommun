1dc2d6b81a3107a612ac9fa693d0c3b603f33ebb
Jonas Hjalmarsson <jonas.hjalmarsson@gmail.com>
2022-08-21 18:36:25 +0200

    color change on driftstörning and wysiwyg field as description

--- inc/acf/driftstorning.php
+++ inc/acf/driftstorning.php
@@ -100,7 +100,7 @@ acf_add_local_field_group(array(
                             'key' => 'field_6301dac816564',
                             'label' => 'Beskrivning',
                             'name' => 'description',
-                            'type' => 'textarea',
+                            'type' => 'wysiwyg',
                             'instructions' => '',
                             'required' => 0,
                             'conditional_logic' => 0,
