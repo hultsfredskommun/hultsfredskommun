dbac1b6f1c06f8ec0c743da038d13dd58d87b3a1
Jonas Hjalmarsson <jonas.hjalmarsson@gmail.com>
2022-09-01 11:34:59 +0200

    header font style changed

--- header.php
+++ header.php
@@ -130,10 +130,7 @@ if ($meta_description != "") :?>
 <?php endif; ?>
 
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-<!--[if lt IE 9]>
-<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
-<link rel="stylesheet" type="text/css" media="all" href="<?php echo get_template_directory_uri() . "/style-lt-ie9.css"; ?>" />
-<![endif]-->
+
 <?php
 	/* We add some JavaScript to pages with the comment form
 	 * to support sites with threaded comments (when in use).
