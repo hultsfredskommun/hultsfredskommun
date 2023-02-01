4e5cd23824e09cfcd95d46b3ba63a0c919a23b34
Jonas Hjalmarsson <jonas@byjalma.se>
2023-02-01 17:28:15 +0100

    rek.ai test på 404 sidan

--- 404.php
+++ 404.php
@@ -11,12 +11,32 @@ get_header(); ?>
 	<div id="top-nav-sidebar">
 		<?php dynamic_sidebar('nav-sidebar'); ?>
 	</div>
-	<aside id='nav' class=' category-navigation' role='navigation'>&nbsp;</aside>
 
-	<div id="primary" class="primary">
+	<div id="primary" class="primary" style='width: 100%'>
 		<div id="content" role="main">
-
 		<?php if (function_exists("hk_404")) hk_404(); ?>
+
+		<style>
+		.rek-prediction__list.rekai-pills  {
+			display: block!important;
+			margin-left: 12px;
+		}		
+
+		.rek-prediction__item.rekai-pill  {
+			/* color: #ed553b;
+			border-radius: 200px; */
+			margin-bottom: 8px;
+			display: block;
+			border: 0;
+		}
+		.rek-prediction__item.rekai-pill a {
+			/* color: #000;
+			text-decoration: none;
+			background: #e5e6e7; */
+			padding: 0;
+		}
+		</style>
+		<div class="rek-prediction" data-renderstyle="pills" data-nrofhits="10" data-titlemaxlength="60"></div>
 
 
 		</div><!-- #content -->
